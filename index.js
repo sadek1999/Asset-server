b3a50f5b0a8ddfdf944ef3bd55b5b5f28e75c0d0
Sadek Ali <sadekali1530@gmail.com>
2023-11-29 22:54:18 +0600

    set user get api

--- index.js
+++ index.js
@@ -32,6 +32,41 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    const assetCollection = client.db('AssetDB').collection('assets')
+    const userCollection=client.db('AssetDB').collection('users')
+
+    // Api for user-----------------------------------
+    // -------------------------------------------------
+    app.post('/user',async(req,res)=>{
+      const user=req.body;
+      const result=await userCollection.insertOne(user);
+      res.send(result)
+    })
+    app.get('/user',async(req,res)=>{
+      let query={};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result=await userCollection.find(query).toArray()
+      res.send(result)
+    })
+    
+
+
+// Api for Assets--------------------------------------------------
+    app.post('/asset',async(req,res)=>{
+      const asset=req.body;
+      const result=await assetCollection.insertOne(asset);
+      res.send(result)
+    })
+    app.get("/asset",async(req,res)=>{
+    
+      const result= await assetCollection.find().sort({ quantity: -1 }).toArray();
+      res.send(result)
+    })
+
+
+
     await client.connect();
     
     // Send a ping to confirm a successful connection
