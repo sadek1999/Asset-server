138c726583eef2fa6376e1c935a9a436d4387f45
Sadek Ali <sadekali1530@gmail.com>
2023-12-01 00:52:19 +0600

    add asset delet api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app=express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const port = process.env.PORT || 8080;
@@ -34,6 +34,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     const assetCollection = client.db('AssetDB').collection('assets')
     const userCollection=client.db('AssetDB').collection('users')
+    const emCollection=client.db('AssetDB').collection('employees')
 
     // Api for user-----------------------------------
     // -------------------------------------------------
@@ -44,10 +45,59 @@ async function run() {
     })
     app.get('/user',async(req,res)=>{
       let query={};
+     
       if (req.query?.email) {
+       
         query = { email: req.query.email }
       }
+      
       const result=await userCollection.find(query).toArray()
+      res.send(result)
+    })
+    // api for employees.-----------------------------------------
+    app.post('/employee',async(req,res)=>{
+      const query=req.body;
+      const result=await emCollection.insertOne(query)
+      res.send(result)
+    })
+    app.get('/employee',async(req,res)=>{
+      let query={};
+      if(req.query?.company){
+        query={company:req.query.company}
+      }
+      if(req.query?.haired){
+        
+        
+        query={haired:req.query.haired}
+      }
+      // console.log(query)
+
+      const result=await emCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.patch('/employee/:id',async(req,res)=>{
+      const id=req.params.id;
+      
+      const query={_id:new ObjectId(id)}
+      const update={
+        $set:{
+          haired:"true"
+        }
+      }
+      const result=await emCollection.updateOne(query,update)
+      res.send(result)
+    })
+
+    app.patch('/employee/remove/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const update={
+        $set:{
+          haired:"false"
+        }
+      }
+      const result=await emCollection.updateOne(query,update)
       res.send(result)
     })
     
@@ -59,9 +109,25 @@ async function run() {
       const result=await assetCollection.insertOne(asset);
       res.send(result)
     })
+
     app.get("/asset",async(req,res)=>{
+      let query={};
+      if(req.query?.name){
+        query={name:req.query.name}
+      }
+      if(req.query?.type){
+        query={type:req.query.type}
+      }
+      // console.log(query)
     
-      const result= await assetCollection.find().sort({ quantity: -1 }).toArray();
+      const result= await assetCollection.find(query).sort({ quantity: -1 }).toArray();
+      res.send(result)
+    })
+
+    app.delete('/asset/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result= await assetCollection.deleteOne(query);
       res.send(result)
     })
 
